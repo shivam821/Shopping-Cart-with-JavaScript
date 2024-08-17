32cafef378865ab05c787ddb21812a1fe5ffa420
shivam821 <73484687+shivam821@users.noreply.github.com>
2024-08-17 21:48:16 +0530

    Commit

--- cart.js
+++ cart.js
@@ -1,3 +1,5 @@
+let label = document.getElementById('label');
+let ShoppingCart = document.getElementById('shopping-cart');
 let basket = JSON.parse(localStorage.getItem('Store Data')) || [];
 
 const calculation = () => {
@@ -6,3 +8,20 @@ const calculation = () => {
 
 };
 calculation();
+
+let generateCartItems = () => {
+    if (basket.length !== 0) {
+        console.log('Basket is not empty');
+    }
+    else {
+        ShoppingCart.innerHTML = ``
+        label.innerHTML = `
+        <h2>Shopping Cart is empty</h2>
+        <a href = 'index.html'>
+            <button class = 'HomeBtn'>Home</button>
+        </a>
+        `;
+    }
+}
+
+generateCartItems();
